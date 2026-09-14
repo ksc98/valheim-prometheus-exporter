350404b6b9435ff3b6093c7cae28a9a5af660c7d
ksc98 <kylechang96@gmail.com>
2026-09-14 14:29:01 -0700

    0.1.4: version strings (the 0.1.4 zip was first published with the 0.1.3 label)

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -15,7 +15,7 @@ namespace ValheimPrometheusExporter
     {
         public const string Guid = "dev.ksc98.valheim-prometheus-exporter";
         public const string Name = "ValheimPrometheusExporter";
-        public const string Version = "0.1.3";
+        public const string Version = "0.1.4";
 
         public static ManualLogSource Log;
 
